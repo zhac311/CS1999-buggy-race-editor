220bfbb233ba5a05630b335c060159376d4a3f77
Dave Whiteland <davewhiteland@users.noreply.github.com>
2020-05-03 19:29:03 +0100

    add basic buggy editor

---
+++ app.py
@@ -0,0 +1,104 @@
+from flask import Flask, render_template, request, jsonify
+import sqlite3 as sql
+app = Flask(__name__)
+
+DATABASE_FILE = "database.db"
+DEFAULT_BUGGY_ID = "1"
+
+#------------------------------------------------------------
+# the index page
+#------------------------------------------------------------
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+#------------------------------------------------------------
+# creating a new buggy:
+#  if it's a POST request process the submitted data
+#  but if it's a GET request, just show the form
+#------------------------------------------------------------
+@app.route('/new', methods = ['POST', 'GET'])
+def create_buggy():
+  if request.method == 'GET':
+    return render_template("buggy-form.html")
+  elif request.method == 'POST':
+    msg=""
+    try:
+      qty_wheels = request.form['qty_wheels']
+      msg = f"qty_wheels={qty_wheels}" 
+      with sql.connect(DATABASE_FILE) as con:
+        cur = con.cursor()
+        cur.execute("UPDATE buggies set qty_wheels=? WHERE id=?", (qty_wheels, DEFAULT_BUGGY_ID))
+        con.commit()
+        msg = "Record successfully saved"
+    except:
+      con.rollback()
+      msg = "error in update operation"
+    finally:
+      con.close()
+      return render_template("updated.html", msg = msg)
+
+#------------------------------------------------------------
+# a page for displaying the buggy
+#------------------------------------------------------------
+@app.route('/buggy')
+def show_buggies():
+  con = sql.connect(DATABASE_FILE)
+  con.row_factory = sql.Row
+  cur = con.cursor()
+  cur.execute("SELECT * FROM buggies")
+  record = cur.fetchone(); 
+  return render_template("buggy.html", buggy = record)
+
+#------------------------------------------------------------
+# a page for displaying the buggy
+#------------------------------------------------------------
+@app.route('/new')
+def edit_buggy():
+  return render_template("buggy-form.html")
+
+
+#------------------------------------------------------------
+# get JSON from current record
+#   this is still probably right, but we won't be
+#   using it because we'll be dipping diectly into the
+#   database
+#------------------------------------------------------------
+@app.route('/json')
+def summary():
+  con = sql.connect(DATABASE_FILE)
+  con.row_factory = sql.Row
+  cur = con.cursor()
+  cur.execute("SELECT * FROM buggies WHERE id=? LIMIT 1", (DEFAULT_BUGGY_ID))
+  return jsonify(
+      {k: v for k, v in dict(zip(
+        [column[0] for column in cur.description], cur.fetchone())).items()
+        if (v != "" and v is not None)
+      }
+    )
+
+#------------------------------------------------------------
+# delete the buggy
+#   don't want DELETE here, because we're anticipating
+#   there always being a record to update (because the
+#   student needs to change that!)
+#------------------------------------------------------------
+@app.route('/delete', methods = ['POST'])
+def delete_buggy():
+  try:
+    msg = "deleting buggy"
+    with sql.connect(DATABASE_FILE) as con:
+      cur = con.cursor()
+      cur.execute("DELETE FROM buggies")
+      con.commit()
+      msg = "Buggy deleted"
+  except:
+    con.rollback()
+    msg = "error in delete operation"
+  finally:
+    con.close()
+    return render_template("updated.html", msg = msg)
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
---
+++ init_db.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+DATABASE_FILE = "database.db"
+
+# important:
+#-------------------------------------------------------------
+# This script initialises your database for you using SQLite,
+# just to get you started... there are better ways to express
+# the data you're going to need... especially outside SQLite.
+# For example... maybe flag_pattern should be an ENUM (which
+# is available in most other SQL databases), or a foreign key
+# to a pattern table?
+#
+# Also... the name of the database (here, in SQLite, it's a
+# filename) appears in more than one place in the project.
+# That doesn't feel right, does it?
+#
+#-------------------------------------------------------------
+
+con = sqlite3.connect(DATABASE_FILE)
+print("- Opened database successfully in file \"{}\"".format(DATABASE_FILE))
+
+# using Python's triple-quote for multi-line strings:
+
+con.execute("""
+
+  CREATE TABLE buggies (
+    id                    INTEGER PRIMARY KEY,
+    qty_wheels            INTEGER DEFAULT 4,
+    flag_color            VARCHAR(20),
+    flag_color_secondary  VARCHAR(20),
+    flag_pattern          VARCHAR(20)
+  )
+
+""")
+
+print("- Table \"buggies\" created successfully")
+
+cur = con.cursor()
+cur.execute("INSERT INTO buggies (qty_wheels) VALUES (4)")
+con.commit()
+
+print("- Added one 4-wheeled buggy")
+print("- done")
+
+con.close()
